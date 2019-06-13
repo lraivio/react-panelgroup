df7212bc43149b611f79b8ab588a3a0d1e44bc26
Lauri Raivio <lauri.raivio@gmail.com>
2019-06-13 12:51:14 +0300

    Remove umd build

--- nwb.config.js
+++ nwb.config.js
@@ -2,11 +2,5 @@ module.exports = {
   type: 'react-component',
   npm: {
     esModules: true,
-    umd: {
-      global: 'ReactPanelGroup',
-      externals: {
-        react: 'React'
-      }
-    }
   }
 }
